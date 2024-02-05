b8a4513a3e962936fe9a18746f56709352030a4c
Ricardo Arias <reariast128@gmail.com>
2024-02-05 16:50:04 -0500

    Created the validator.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import re
 
+expression = "\w+@{1}[a-zA-Z]+\.{1}[a-zA-Z]+"
+email = input("Type your email -> ")
+
+validator = re.fullmatch(expression, email)
+if validator is not None:
+    print(f"Your email parsed is -> {validator.string}")
+else:
+    print("Not a valid email!")
